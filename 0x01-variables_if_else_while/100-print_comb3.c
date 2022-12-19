59003a032119f37b450faaf8cdca10ae227f6c15
DeeBryce <deeamong@gmail.com>
2022-12-18 16:12:21 -0800

    fixes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
+			
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 			
@@ -25,6 +25,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 }
